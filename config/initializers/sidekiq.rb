816bce0563801f5a9e4437c1a4ced99ded145c8c
Matt Darby <darby@mittdarko.com>
2014-09-10 16:12:51 -0400

    let ENVs work

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,10 +4,3 @@ require 'sidekiq/web'
 Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
   [user, password] == ["mdarby", "sidekiq"]
 end
-
-if Rails.env.production?
-  Sidekiq.configure_server do |config|
-    config.redis = { :url => 'redis://172.17.0.26:49154/12', :namespace => 'crb_jobs' }
-
-  end
-end
