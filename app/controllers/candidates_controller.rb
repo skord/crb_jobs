aa9b3b050540c1c2f3767db7dc1c17dd6804ebd1
Matt Darby <darby@mittdarko.com>
2014-09-10 13:56:55 -0400

    Params

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -5,4 +5,10 @@ class CandidatesController < InheritedResources::Base
     Candidate.where(token: params[:token]).first.update_attribute(:receive_updates, false)
   end
 
+  private
+
+  def permitted_params
+    params.permit(candidate: [:first_name, :last_name, :email, :github_name, :twitter_name, :experience_level, :about_me, :looking_for, :receive_updates])
+  end
+
 end
